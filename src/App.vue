eb62366f4625aff4bee4cd6ed3e0174f3370159f
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2019-01-03 15:40:55 -0600

    adds items to the list

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,15 @@
         <v-layout justify-center align-center>
           <v-flex xs12 sm6>
             <v-text-field
+              @keyup.enter="addTodo"
               v-model="newTodo"
               label="Add a todo"
               outline
               clearable
             ></v-text-field>
+            <ul v-for="todo in todos" :key="todo.id">
+              <li>{{ todo.text }}</li>
+            </ul>
           </v-flex>
         </v-layout>
       </v-container>
@@ -20,8 +24,36 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  name: 'App',
-};
+<script lang="ts">
+import { Component, Vue, Model, Prop, Emit } from 'vue-property-decorator'
+
+interface Todo {
+  id: number
+  text: string
+  isCompleted: boolean
+}
+
+@Component
+export default class App extends Vue {
+  @Model('input', { type: String, default: '' }) todoInput!: string
+
+  private todos: Todo[] = []
+  private newTodo: string = `${this.todoInput}`
+
+  @Emit()
+  addTodo() {
+    const value = this.newTodo && this.newTodo.trim()
+
+    if (!value) {
+      return
+    }
+    this.todos.push({
+      id: this.todos.length,
+      text: value,
+      isCompleted: false,
+    })
+
+    this.newTodo = ''
+  }
+}
 </script>
