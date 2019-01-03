4fb222f073a9f402f4ec1618efe82ae5479e7db0
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2019-01-03 12:52:34 -0600

    vuetify?

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <div id="app"><HelloWorld msg="Todo App" /></div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content> <HelloWorld /> </v-content>
+  </v-app>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import HelloWorld from './components/HelloWorld';
 
-@Component({
+export default {
+  name: 'App',
   components: {
     HelloWorld,
   },
-})
-export default class App extends Vue {}
+  data() {
+    return {
+      //
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
