a02eaaa8b8f36ece2d80a43d01fbca8183f4a3e6
Andres Rodriguez <arodriguez@coolfiresolutions.com>
2019-01-03 09:26:25 -0600

    prettier :all_the_things:!

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <div id="app"><HelloWorld msg="Todo App" /></div>
 </template>
 
 <script lang="ts">
